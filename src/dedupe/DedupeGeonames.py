78452e8cad1d119f32d1c96835fbfc11bacc11dd
J.T <gisfromscratch@live.de>
2018-09-22 17:30:29 +0200

    Handson geonames using LatLong comparison, geometry None is not allowed

--- src/dedupe/DedupeGeonames.py
+++ src/dedupe/DedupeGeonames.py
@@ -15,6 +15,9 @@ import sys
 import argparse as ag
 import dedupe
 from unidecode import unidecode
+import traceback as tb
+import psutil as pu
+#import objgraph as og
 
 # ## Logging
 logger = logging.getLogger('dedupe')
@@ -35,10 +38,12 @@ def preProcess(column):
         column = None
     return column
     
+    """
     try : # python 2/3 string differences
         column = column.decode('utf8')
     except AttributeError:
         pass
+    """
     column = unidecode(column)
     column = re.sub('  +', ' ', column)
     column = re.sub('\n', ' ', column)
@@ -52,6 +57,7 @@ def readData(filename, encoding, delimiter, header, keyfield, maxRecordCount=Non
     """
     Read in our data from a CSV file and create a dictionary of records, 
     where the key is a unique record ID and each value is dict
+    If no latitude or longitude exists the record is thrown away.
     """
 
     data_d = {}
@@ -60,17 +66,26 @@ def readData(filename, encoding, delimiter, header, keyfield, maxRecordCount=Non
         reader = csv.DictReader(f, fieldnames=header, delimiter=delimiter, quoting=csv.QUOTE_NONE)
         for row in reader:
             clean_row = [(k, preProcess(v)) for (k, v) in row.items()]
-            row_id = int(row[keyfield])
-            data_d[row_id] = dict(clean_row)
-            recordCount += 1
-            if (maxRecordCount and maxRecordCount == recordCount):
-                return data_d
+            latitude = None
+            longitude = None
+            for (k, v) in clean_row:
+                if 'latitude' == k:
+                    latitude = float(v)
+                if 'longitude' == k:
+                    longitude = float(v)
+            if latitude and longitude:
+                clean_row.append(('geometry', (latitude, longitude)))
+                row_id = int(row[keyfield])
+                data_d[row_id] = dict(clean_row)
+                recordCount += 1
+                if (maxRecordCount and maxRecordCount == recordCount):
+                    return data_d
 
     return data_d
 
 # ## Parsing arguments
 argParser = ag.ArgumentParser(description='Train a deduplication model for record linkage using Geonames')
-argParser.add_argument('--input', default='D:/Geonames/geonames_modifications_extract.tsv', nargs='?', type=str, help='Path to the labeled geonames file containing duplicates.')
+argParser.add_argument('--input', default='D:/Geonames/geonames_modifications_sorted.tsv', nargs='?', type=str, help='Path to the labeled geonames file containing duplicates.')
 argParser.add_argument('--train', default='D:/Geonames/train.json', nargs='?', type=str, help='Output path to training file.')
 argParser.add_argument('--settings', default='D:/Geonames/train.settings', nargs='?', type=str, help='Output path to settings file.')
 arguments = argParser.parse_args(sys.argv[1:])
@@ -80,33 +95,38 @@ else:
     argParser.print_help()
     sys.exit(1)
 
-
 try:
+    # ## Log memory infos
+    process = pu.Process(os.getpid())
+    memInfo = process.memory_info()
+    logger.info('Memory usage rss: {} MB, vms: {} MB'.format(memInfo.rss/1024/1024, memInfo.vms/1024/1024))
+
     # ## Import training data
     logger.info('Reading training data')
     filename = arguments.input
     header = ['id', 'geonameid','name','asciiname','alternatenames','latitude','longitude','feature class','feature code','country code','cc2','admin1 code','admin2 code','admin3 code','admin4 code','population','elevation','dem','timezone','modification date']
     keyfield = 'id'
-    maxRecordCount = None
+    maxRecordCount = 50000
     trainingData = readData(filename, 'utf-8', '\t', header, keyfield, maxRecordCount)
     
     # ## Training
     fields = [
         { 'field':'name', 'type':'String' },
         { 'field':'asciiname', 'type':'String' },
-        { 'field':'latitude', 'type':'String' },
-        { 'field':'longitude', 'type':'String' },
+        { 'field':'geometry', 'type':'LatLong', 'has missing':True },
         { 'field':'country code', 'type':'Exact', 'has missing':True }
     ]
     commonField = 'geonameid'
     
     # Create labeled data
-    labeledData = dedupe.trainingDataDedupe(trainingData, commonField)
+    trainingSize = int(0.8*len(trainingData))
+    labeledData = dedupe.trainingDataDedupe(trainingData, commonField, training_size=trainingSize)
     
     # Create the matcher
     logger.info('Train using the labeled data')
     matcher = dedupe.Dedupe(fields)
-    matcher.sample(trainingData)
+    sampleSize = int(0.2*len(trainingData))
+    matcher.sample(trainingData, sample_size=sampleSize)
     matcher.markPairs(labeledData)
     matcher.train()
     logger.info('Training finished')
@@ -123,10 +143,16 @@ try:
     with open(settingsFile, 'wb') as sf:
         matcher.writeSettings(sf)
         
+    memInfo = process.memory_info()
+    logger.info('Memory usage rss: {} MB, vms: {} MB'.format(memInfo.rss/1024/1024, memInfo.vms/1024/1024))
+    
     matcher.cleanupTraining()
     del matcher
 except Exception as e:
-    logger.error(str(e))
+    logger.exception("Dedupe failed badly!")
+    memInfo = process.memory_info()
+    logger.info('Memory usage rss: {} MB, vms: {} MB'.format(memInfo.rss/1024/1024, memInfo.vms/1024/1024))
+    #og.show_most_common_types(file=open('leaks.txt', 'w+'))
     
     
     # ## Import real data
