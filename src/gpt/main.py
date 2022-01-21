cfd8d6fc357be12d32e387000ecd433db81e0681
gisfromscratch <gisfromscratch@live.de>
2022-01-21 12:48:02 +0100

    Added some urls

--- src/gpt/main.py
+++ src/gpt/main.py
@@ -1,6 +1,12 @@
 import io
+import json
+import newspaper
+from newspaper import Article
+import nltk
+import random
 import spacy
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+import urllib.request
 import uuid
 
 class TextGenerator(object):
@@ -8,6 +14,7 @@ class TextGenerator(object):
     def prepare_generation(self):
         print('Loading model...')
         self._model = GPTNeoForCausalLM.from_pretrained('EleutherAI/gpt-neo-125M', cache_dir='D:/Data/ML/cache')
+        #self._model = GPT2LMHeadModel.from_pretrained.from_pretrained('openai-gpt', cache_dir='D:/Data/ML/cache')
         print('Loading model done.')
         print('Loading tokenizer...')
         self._tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-125M', cache_dir='D:/Data/ML/cache')
@@ -15,14 +22,66 @@ class TextGenerator(object):
 
     def prepare_parsing(self):
         self._nlp = spacy.load('en_core_web_sm')
+        nltk.download('punkt')
+
+    def parse_articles(self, url):
+        web_newspaper = newspaper.build(url, memoize_articles=False)
+        articles = []
+        for article in web_newspaper.articles:
+            try:
+                article.download()
+                article.parse()
+                article.nlp()
+                articles.append(article)
+            except:
+                pass
+            
+        return articles
+
+    def query_articles(self, query):
+        query_params = {
+            'query': query,
+            'mode': 'artlist',
+            'timespan': '1day',
+            'sort': 'datedesc',
+            'format': 'json'
+        }
+        query_string = urllib.parse.urlencode(query_params)
+        gdelt_request = urllib.request.Request('https://api.gdeltproject.org/api/v2/doc/doc?{0}'.format(query_string),
+                                headers={'content-type': 'application/json'})
+        web_response = json.loads(urllib.request.urlopen(gdelt_request).read().decode('utf8'))
+        gdelt_articles = []
+        article_titles = {}
+        if 'articles' in web_response:
+            for article_entry in web_response['articles']:
+                if ('title' in article_entry
+                    and 'url' in article_entry):
+                    web_articles = self.parse_articles(article_entry['url'])
+                    gdelt_articles.extend(web_articles)
+                    # Just add the unparsed one
+                    '''
+                    if not article_entry['title'] in article_titles:
+                        gdelt_articles.append(article_entry)
+                        article_titles[article_entry['title']] = article_entry
+                    '''
+
+        return gdelt_articles               
 
     def generate(self, input_text):
         print('Tokenize text...')
+        max_text_length = 1024
+        if max_text_length < len(input_text):
+            input_text = input_text[:max_text_length]
         input_ids = self._tokenizer(input_text, return_tensors='pt').input_ids
         print('Tokenize text done.')
 
         print('Generate text...')
-        gen_tokens = self._model.generate(input_ids, do_sample=True, temperature=0.9, max_length=100)
+        input_length = input_ids.shape[1]
+        if 1.25*input_length < 100:
+            max_length = 100
+        else:
+            max_length = random.randint(int(1.25*input_length), int(1.5*input_length))
+        gen_tokens = self._model.generate(input_ids, do_sample=True, temperature=0.9, max_length=max_length)
         print('Generate text done.')
         print('Decode text...')
         gen_text = self._tokenizer.batch_decode(gen_tokens)[0]
@@ -61,19 +120,23 @@ class TextBuilder(object):
 
         return True
 
+    def get_last_sentence(self):
+        if len(self._sentences) < 1:
+            return self.to_text()
+
+        return self.to_text(self._sentences[-1:])
+    
     def pop_last_sentence(self):
         text = self.to_text(self._sentences[-1:])
         self._sentences = self._sentences[:-1]
         return text
 
     def pop_last_sentences(self):
-        if len(self._sentences) < 3:
-            text = self.to_text(self._sentences[1:])
-            self._sentences = self._sentences[:1]
-            return text
+        if len(self._sentences) < 4:
+            return self.pop_last_sentence()
 
-        text = self.to_text(self._sentences[-2:])
-        self._sentences = self._sentences[:-2]
+        text = self.to_text(self._sentences[-3:])
+        self._sentences = self._sentences[:-3]
         return text
 
     def pop_sentences(self):
@@ -96,41 +159,53 @@ class TextBuilder(object):
 if __name__ == '__main__':
     #input_text = 'A geographic information system (GIS) is a system that creates, manages, analyzes, and maps all types of data. GIS connects data to a map, integrating location data and make the world a better place. Thats is our promise.'
     #input_text = input('Input text:')
-    with io.open('input.txt', 'r', encoding='utf8') as in_stream:
-        input_text = ' '.join(in_stream.read().split())
+    input_lines = []
 
-    max_sentences = 50
-    builder = TextBuilder(max_sentences=50)
+    '''
+    with io.open('input.txt', 'r', encoding='utf8') as in_stream:
+        input_lines = [' '.join(line.split()) for line in in_stream.readlines()]
+        #input_text = ' '.join(in_stream.read().split())
+    '''
+
+    max_sentences = 10
     generator = TextGenerator()
     generator.prepare_generation()
     generator.prepare_parsing()
 
-    input_sentences = generator.validate(input_text)
-    sentences_count = len(input_sentences)
-    paragraph_count = int(max_sentences/sentences_count)
-    if paragraph_count < 1:
-        paragraph_count = 1
+    query = '"location intelligence" AND sourcelang:eng AND repeat3:"location"'
+    #query = '"Germany" AND sourcelang:eng AND repeat3:"location"'
+    gdelt_articles = generator.query_articles(query)
+    article_count = len(gdelt_articles)
     
-    print('sentences: ', sentences_count)
-    print('paragraphs: ', paragraph_count)
+    print('articles: ', article_count)
 
-    generate = True
-    for sentence_index in range(sentences_count):
-        if not generate:
-            break
+    new_uuid = str(uuid.uuid4()).replace('-', '')
+    out_file = 'generated_{0}.txt'.format(new_uuid)
+    for article_index in range(article_count):
+        article = gdelt_articles[article_index]
+        input_text = article.title
+        if len(input_text) < 10:
+            print('Text too short!')
+            continue
 
-        input_text = str(input_sentences[sentence_index])
-        for paragraph_index in range(paragraph_count):
-            if not generate:
-                break
-            
+        with io.open(out_file, 'a', encoding='utf8') as out_stream:
+            out_stream.write(article.title)
+            out_stream.write('\n')
+            out_stream.write(article.url)
+            out_stream.write('\n')
+            out_stream.write(article.summary)
+            out_stream.write('\n\n')
+
+        generate = True
+        builder = TextBuilder(max_sentences)
+        while generate:
             generated_text = generator.generate(input_text)
             #generated_text = input_text
-            '''
+            
             print('### Generated ###')
             print(generated_text)
             print('### End of Generated ###')
-            '''
+            
             sentences = generator.validate(generated_text)
             if None is sentences:
                 print('Sentences could not be generated!')
@@ -142,18 +217,17 @@ if __name__ == '__main__':
                     print('### Abstract ###')
                     print(builder.to_text())
                     print('### End of Abstract ###')
-                    '''
                     print('### Input ###')
-                    print(input_text)
+                    print(last_text)
                     print('### End of Input ###')
+                    '''
                 else:
                     generate = False
 
-    print()
-    print(builder.to_text())
-    print()
-
-    new_uuid = str(uuid.uuid4()).replace('-', '')
-    out_file = 'generated_{0}.txt'.format(new_uuid)
-    with io.open(out_file, 'w', encoding='utf8') as out_stream:
-        out_stream.write(builder.to_ptext())
+        print()
+        print(builder.to_text())
+        print()
+    
+        with io.open(out_file, 'a', encoding='utf8') as out_stream:
+            out_stream.write(builder.to_ptext())
+            out_stream.write('\n### End of Article ###\n\n')
