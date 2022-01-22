9b5c3fbf1351cc593b04865a35e48b557c0791b1
gisfromscratch <gisfromscratch@live.de>
2022-01-22 18:31:31 +0100

    Reduced text breaks, leads to better results

--- src/gpt/paraphrase.py
+++ src/gpt/paraphrase.py
@@ -1,13 +1,15 @@
 # conda install sentencepiece
 import spacy
+import sys
 import torch
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+
 
 
 model_name = 'tuner007/pegasus_paraphrase'
 torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
-model = PegasusForConditionalGeneration.from_pretrained(model_name, cache_dir='D:/Data/ML/cache').to(torch_device)
+model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
 
 def get_response(input_text, num_return_sequences, num_beams):
     max_length = 60
@@ -17,19 +19,28 @@ def get_response(input_text, num_return_sequences, num_beams):
     return tgt_text
 
 def paraphrase_paragraph(input_paragraph):
+    min_text_length = 10
     num_beams = 10
     nlp = spacy.load('en_core_web_sm')
     parsed_paragraph = nlp(input_paragraph)
     paraphrased_samples = []
     for sentence in parsed_paragraph.sents:
-        paraphrased_sample = get_response(str(sentence), num_return_sequences=1, num_beams=num_beams)
-        paraphrased_samples.extend(paraphrased_sample)
+        # Reduce all \n\t\s to simple whitespace
+        stripped_sentence = ' '.join(str(sentence).split())
+        if min_text_length <= len(stripped_sentence):
+            paraphrased_sample = get_response(stripped_sentence, num_return_sequences=1, num_beams=num_beams)
+            paraphrased_samples.extend(paraphrased_sample)
     
     return ' '.join(paraphrased_samples)
 
 
 
 if __name__ == '__main__':
-    paragraph = "Mapping the geospatial patterns of broadcasted news allows a geospatial analyst to gain insights into common and unusual geospatial patterns. We decided using one of the most comprehensive news collection named “Global Database Events of Tone and Language” (GDELT) as the ground truth."
+    if len(sys.argv) < 2:
+        paragraph = "Mapping the geospatial patterns of broadcasted news allows a geospatial analyst to gain insights into common and unusual geospatial patterns. We decided using one of the most comprehensive news collection named “Global Database Events of Tone and Language” (GDELT) as the ground truth."
+    else:
+        with open(sys.argv[1], mode='r', encoding='utf-8') as in_stream:
+            paragraph = in_stream.read()
+    
     paraphrased_text = paraphrase_paragraph(paragraph)
     print(paraphrased_text)
